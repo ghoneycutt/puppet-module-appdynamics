561f39207f3135ff6cd8c95b537e42db6cd6391f
Garrett Honeycutt <code@garretthoneycutt.com>
2016-12-15 18:05:17 -0500

    Refactor the module.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,32 +1,38 @@
 # == Class: appdynamics
 #
 class appdynamics (
-  $controller_install_package      = $appdynamics::params::controller_install_package,
-  $controller_config               = $appdynamics::params::controller_config,
-  $iio_port                        = $appdynamics::params::iio_port,
-  $server_port                     = $appdynamics::params::server_port,
-  $server_hostname                 = $appdynamics::params::server_hostname,
-  $ha_controller_type              = $appdynamics::params::ha_controller_type,
-  $controller_tenancy_mode         = $appdynamics::params::controller_tenancy_mode,
-  $admin_port                      = $appdynamics::params::admin_port,
-  $language                        = $appdynamics::params::language,
-  $jms_port                        = $appdynamics::params::jms_port,
-  $install_dir                     = $appdynamics::params::install_dir,
-  $mysql_root_password             = $appdynamics::params::mysql_root_password,
-  $database_port                   = $appdynamics::params::database_port,
-  $username                        = $appdynamics::params::username,
-  $password                        = $appdynamics::params::password,
-  $ssl_port                        = $appdynamics::params::ssl_port,
-  $real_datadir                    = $appdynamics::params::real_datadir,
-  $elasticsearch_datadir           = $appdynamics::params::elasticsearch_datadir,
-  $disable_eula                    = $appdynamics::params::disable_eula,
-  $root_user_password              = $appdynamics::params::root_user_password,
-  $reporting_service_http_port     = $appdynamics::params::reporting_service_http_port,
-  $reporting_service_https_port    = $appdynamics::params::reporting_service_https_port,
-  $elasticsearch_port              = $appdynamics::params::elasticsearch_port,
-  ) inherits appdynamics::params {
+  $controller_install_path      = '/tmp/controller.sh',
+  $ha_toolkit_path              = '/tmp/HA-toolkit.tar.gz',
+  $controller_config            = 'demo',
+  $iio_port                     = '3700',
+  $server_port                  = '8090',
+  $server_hostname              = $::fqdn,
+  $ha_controller_type           = 'notapplicable',
+  $controller_tenancy_mode      = 'single',
+  $admin_port                   = '4848',
+  $language                     = 'en',
+  $jms_port                     = '7676',
+  $install_dir                  = '/home/appduser/AppDynamics/Controller',
+  $mysql_root_password          = 'DRvYYv9eq6',
+  $database_port                = '3388',
+  $username                     = 'admin',
+  $password                     = 'pa55word',
+  $ssl_port                     = '8181',
+  $real_datadir                 = '/home/appduser/AppDynamics/Controller/db/data',
+  $elasticsearch_datadir        = '/home/appduser/AppDynamics/Controller/events_service/analytics-processor',
+  $root_user_password           = 'pa55word2',
+  $reporting_service_http_port  = '8020',
+  $reporting_service_https_port = '8021',
+  $elasticsearch_port           = '9200',
+  $manage_libaio                = true,
+  $manage_gcc                   = true,
+  $manage_unzip                 = true,
+  $install_timeout              = 900,
+  $exec_path                    = '/bin:/usr/bin:/sbin:/usr/sbin',
+) {
 
-  validate_string($controller_install_package)
+  validate_absolute_path($controller_install_path)
+  validate_absolute_path($ha_toolkit_path)
   validate_re($controller_config, '^demo$|^small$|^medium$|^large$|^extra-large$', 'The controller config must be set to demo, small, medium, large, or extra-large')
   validate_string($iio_port)
   validate_string($server_port)
@@ -44,9 +50,123 @@ class appdynamics (
   validate_string($ssl_port)
   validate_string($real_datadir)
   validate_string($elasticsearch_datadir)
-  validate_string($disable_eula)
   validate_string($root_user_password)
   validate_string($reporting_service_http_port)
   validate_string($reporting_service_https_port)
   validate_string($elasticsearch_port)
+  validate_bool($manage_libaio)
+  validate_bool($manage_gcc)
+  validate_bool($manage_unzip)
+  validate_integer($install_timeout)
+  validate_string($exec_path)
+
+  if $manage_libaio == true {
+    package { 'libaio':
+      ensure => installed,
+      before => Exec['install_controller'],
+    }
+  }
+
+  if $manage_gcc == true {
+    package { 'gcc':
+      ensure => installed,
+      before => Exec['install_controller'],
+    }
+  }
+
+  if $manage_unzip == true {
+    package { 'unzip':
+      ensure => installed,
+      before => Exec['install_ha_toolkit'],
+    }
+  }
+
+  File_line {
+    before => Exec['install_controller'],
+  }
+
+  file_line { 'appd hard nofile':
+    path => '/etc/security/limits.conf',
+    line => 'appd hard nofile 65535',
+  }
+
+  file_line { 'appd soft nofile':
+    path => '/etc/security/limits.conf',
+    line => 'appd soft nofile 65535',
+  }
+
+  file_line { 'appd hard nproc':
+    path => '/etc/security/limits.conf',
+    line => 'appd hard nproc 8192',
+  }
+
+  file_line { 'appd soft nproc':
+    path => '/etc/security/limits.conf',
+    line => 'appd soft nproc 8192',
+  }
+
+  file_line { 'ulimit config 1':
+    path => '/etc/profile',
+    line => 'ulimit -n 65535',
+  }
+
+  file_line { 'ulimit config 2':
+    path => '/etc/profile',
+    line => 'ulimit -u 8192',
+  }
+
+  file_line { 'pam configuration':
+    path => '/etc/pam.d/system-auth',
+    line => 'session required pam_limits.so',
+  }
+
+  file { 'appdynamics_response':
+    ensure  => 'file',
+    path    => '/opt/response.varfile',
+    content => template('appdynamics/response.varfile.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    before  => Exec['install_controller'],
+  }
+
+  exec { 'install_controller':
+    command   => "sh ${controller_install_path} -q -varfile /opt/response.varfile",
+    creates   => '/home/appduser/AppDynamics/Controller',
+    logoutput => true,
+    timeout   => $install_timeout,
+    path      => $exec_path,
+  }
+
+  file { 'appdynamics_license':
+    ensure  => 'file',
+    path    => "${install_dir}/license.lic",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    #source  => 'puppet:///modules/appdynamics/license.lic',
+    require => Exec['install_controller'],
+  }
+
+  exec { 'install_ha_toolkit':
+    command => "tar -xzvf ${ha_toolkit_path} -C /tmp && mv /tmp/HA-toolkit ${install_dir}/AppDynamicsHA",
+    creates => "${install_dir}/AppDynamicsHA",
+    path    => $exec_path,
+    require => Exec['install_controller'],
+  }
+
+  exec { 'install_init_script':
+    command => "bash ${install_dir}/AppDynamicsHA/install-init.sh",
+    creates => '/etc/init.d/appdcontroller',
+    path    => $exec_path,
+    require => Exec['install_ha_toolkit'],
+    notify  => Service['appdcontroller'],
+  }
+
+  service { 'appdcontroller':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+  }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,8 +1,7 @@
-
 require 'spec_helper'
 describe 'appdynamics' do
-
   context 'with defaults for all parameters' do
+    it { should compile.with_all_deps }
     it { should contain_class('appdynamics') }
   end
 end
